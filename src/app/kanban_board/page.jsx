eb75bb64b498e4ae9d8fab51fbdbe13db2e4cfb6
rajnworx <raj@nworx.ai>
2025-03-21 16:50:27 +0530

    UPDATED:kanban board

---
+++ src/app/kanban_board/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react'
+import KanbanBoard from '@/components/kanban/KanbanBoard';
+
+const page = () => {
+  return ( 
+    <div className='flex'>
+   
+   <KanbanBoard/>
+    </div>
+  )
+}
+
+export default page
+
