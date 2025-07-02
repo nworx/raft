9fa56cc6724f75d4a44728f56f476cea3c0ddbae
rajnworx <raj@nworx.ai>
2025-07-02 12:06:24 +0530

    ADDED:validation check on most of pages

--- src/app/raft/layout.js
+++ src/app/raft/layout.js
@@ -1,19 +1,22 @@
 "use client";
 // import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import React,{useEffect, useLayoutEffect} from 'react';
 
 export default function DashboardLayout({ children }) {
+    const router=useRouter();
 //   const cookieStore = cookies();
 //   const token = cookieStore.get('jwt')?.value;
 
  useLayoutEffect(() => {
     const cookies = document.cookie;
+    
     console.log(cookies,"cookiescookiescookies")
     const hasJWT = cookies.includes('jwt=');
 
     if (!hasJWT) {
-      router.replace('/');
+      router.push('/');
     }
   }, []);
 
