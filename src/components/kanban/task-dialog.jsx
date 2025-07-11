7b9b2117d51b0fd9351d6fd2c972c84dae3787a2
rajnworx <raj@nworx.ai>
2025-07-11 14:58:43 +0530

    ADDED:showing image size  is fixed in tip tap

--- src/components/kanban/task-dialog.jsx
+++ src/components/kanban/task-dialog.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -20,7 +20,6 @@ export default function TaskDialog({ task, open, onOpenChange, onAddComment }) {
   const { toast } = useToast();
 
 
-  console.log(task, task?.description,  "Prakhar 123")
   const handleAddComment = () => {
     console.log(newComment, "newComment")
     if (!newComment.content[0].content[0].text.trim()) {
