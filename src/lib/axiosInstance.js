73a3ec9dc851d8383573ebadd3461bd272739af7
rajnworx <raj@nworx.ai>
2025-07-16 18:54:11 +0530

    UPDATED:api call updated

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,13 +1,28 @@
 "use client"
 import axios from "axios";
 
-const token = document.cookie.match(/(?:^|;\s*)jwt=([^;]*)/)?.[1];
+// const token = document.cookie.match(/(?:^|;\s*)jwt=([^;]*)/)?.[1];
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_NWORX_BASE_URL,
   withCredentials: true, 
-  headers: {
-              Authorization: `Bearer ${token}`, 
-          },
+  // headers: {
+  //             Authorization: `Bearer ${token}`, 
+  //         },
+});
+
+api.interceptors.request.use((config) => {
+  const token = document.cookie.match(/(?:^|;\s*)jwt=([^;]*)/)?.[1];
+
+  // Only attach token for endpoints that are NOT public
+  const isPublic = config.headers?.skipAuth === true || config.url.includes("/signIn","/signUp");
+
+  if (!isPublic && token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization;
+  }
+
+  return config;
 });
 
 export default api;
